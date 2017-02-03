9b8a42eaf4f84e97c49a6564f436c2fb5e41aafa
Brandon Schneider <brandon@ixsystems.com>
2017-02-03 12:19:53 -0600

    We don't need the stderr.

--- post_install.sh
+++ post_install.sh
@@ -5,5 +5,5 @@ sysrc -f /etc/rc.conf syncthing_enable="YES"
 sysrc -f /etc/rc.conf nginx_enable="YES"
 
 # Start the service
-service syncthing start
-service nginx start
+service syncthing start 2>/dev/null
+service nginx start 2>/dev/null
