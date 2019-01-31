1ab7055e914dc1832f36c493ac9728d096725cb6
Martin Wilke <miwi@ixsystems.com>
2019-01-31 14:36:38 +0800

    Create a default directory and give correct permission

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,9 @@
 sysrc -f /etc/rc.conf syncthing_enable="YES"
 sysrc -f /etc/rc.conf nginx_enable="YES"
 
+mkdir /Sync
+chown -R syncthing:syncthing /Sync
+
 # Start the service
 service syncthing start 2>/dev/null
 service nginx start 2>/dev/null
